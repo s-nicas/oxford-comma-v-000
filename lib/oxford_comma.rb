61f8cdd4009c5d3e22582744deac2dd79881b9f2
Sonia  Nicas <sonia.d.nicas@gmail.com>
2018-04-24 04:39:55 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
+  if array.size == 1
+    return array.join 
+  
+  else 
+    and_last = "and #{array.last}" 
+    array.pop
+    array.push(and_last)
 
+     if array.size == 2 
+      return array.join(' ')
+    
+    elsif array.size>2
+      return array.join(', ')
+    end
+  end 
 end
